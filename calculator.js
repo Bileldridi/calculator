5b5c340de115f9f998aef53af6c9dc4eabc61233
bilel dridi <drd.bilel@gmail.com>
2020-08-10 11:15:46 +0100

    calculator

---
+++ calculator.js
@@ -0,0 +1,21 @@
+function insert(num) {
+    var a = document.form.textview;
+    a.value += num;
+}
+
+function equal() {
+    var a = document.form.textview;
+    var exp = a.value;
+    if (exp) {
+        a.value = eval(exp);
+    }
+}
+
+function clean() {
+    document.form.textview.value = '';
+}
+
+function back() {
+    var exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0, exp.length-1);
+}
